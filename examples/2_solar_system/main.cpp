67728b329d1cd26009bc9e690d28e2138061b4a7
Jari Daemen <jaridaemen2@gmail.com>
2026-07-14 14:29:01 +0200

    Refactor

--- examples/2_solar_system/main.cpp
+++ examples/2_solar_system/main.cpp
@@ -4,11 +4,16 @@
 #include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_log.h>
 #include "matrix.h"
 #include <iostream>
 #include <vector>
 #include <math.h>
 
+const Vertex CAMERA_POS = Vertex {.x = 0, .y = 0, .z = 0, .w = 1}; // camera placed at origin of world coord system for convenience
+const int W = 800;
+const int H = 800;
+const float FOV = 90; // in degrees
 
 /*
  * Input -> vertex v in NDC coordinates
@@ -60,111 +65,242 @@ Vertex toNDC(Vertex v, int W, int H){
   return v_NDC;    
 }
 
+
+//
+//
+//
+//
+//
+//
+//
+//
+struct Face {
+    public:
+        std::vector<Vertex> vts {};
+        Face(std::vector<Vertex> vts_in){
+            for(auto& v : vts_in){
+                vts.push_back(Vertex {.x = v.x, .y = v.y, .z = v.z, .w = v.w});
+            }
+        };
+        
+        //
+        Face translate_face(Vertex offset){
+            std::vector<Vertex> vts_cp{};
+            for(int i = 0; i < vts.size() ; i++){
+                Vertex v_cp{};
+                v_cp.x = vts.at(i).x + offset.x;
+                v_cp.y = vts.at(i).y + offset.y;
+                v_cp.z = vts.at(i).z + offset.z;
+                v_cp.w  = 1;
+
+                vts_cp.push_back(v_cp);
+            }
+            return Face(vts_cp);
+        }
+   protected:
+        //
+   private:
+        //
+};
+
+struct Geometry {
+    protected:
+        std::vector<Face> faces{};
+        //
+        Geometry(std::vector<Face> faces_in){
+        for(auto& f : faces_in)
+            {
+                faces.push_back(f);
+            };
+        };
+        /*
+         * n = 4 -> cube
+         *
+         */
+        Geometry(std::vector<Vertex> vts_in, int n){
+            if(vts_in.size() % n != 0) {
+                std::cout << "ERR: Amount of supplied vertices must be a multiple of n!" << '\n';
+                throw;
+            }
+            std::vector<Vertex> tmp{};
+            for(Vertex v : vts_in){
+                tmp.push_back(v);
+                if(tmp.size() % n == 0){
+                    faces.push_back(Face(tmp));
+                    //
+                    tmp = {};
+                }
+            }
+        }
+        //
+     public:
+        std::vector<Vertex> extractVts(){
+            std::vector<Vertex> vts{};
+            for(Face f : faces){
+                for(Vertex v : f.vts){
+                    vts.push_back(v);
+                }
+            }
+            return vts;
+        }
+
+        void printProperties(){
+            std::vector<Vertex> vts = extractVts();
+            std::cout << "------WORLD SPACE COORDS.--------" << '\n';
+            for(int i = 0; i < vts.size(); i++){
+                 Vertex v = vts.at(i);
+                 std::cout << '(' << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ')';
+                 std::cout << '\n';
+                 if((i+1) % 4 == 0){
+                   std::cout << '\n';
+                 }
+            }
+            std::cout << "--------------------------------" << '\n';
+        };
+
+        // Angle in degrees
+        void rotate_geom(float angle, int axis){        
+                for(int i = 0; i < faces.size() ; i++){
+                    for(int j = 0; j < faces.at(0).vts.size(); j++){
+                        faces.at(i).vts.at(j) = rot(faces.at(i).vts.at(j), angle, axis);
+                    }
+                }
+       };
+      
+       //
+       //Vertex v_t = translate(v, offset_vector);
+       void translate_geom(Vertex offset_vertex){
+         for(int i = 0; i < faces.size(); i++){
+            //f.translate_face(offset_vertex);
+            faces.at(i) = faces.at(i).translate_face(offset_vertex);
+         }  
+       }
+};
+
+struct Cube : Geometry {
+    // Overload constructor 
+    Cube(std::vector<Face> faces_in) : Geometry(faces_in){};
+    Cube(std::vector<Vertex> vts_in) : Geometry(vts_in, 4){};    
+
+    //
+    void render(SDL_Renderer* renderer){
+        SDL_SetRenderDrawColor(renderer, 10, 245, 37,SDL_ALPHA_OPAQUE); // Green
+        
+        /*
+        // project to near plane, then map to screen
+        for(int i = 0 ; i < faces_cp.size() ; i++){
+            for(int j = 0 ; j < faces_cp.at(i).vts.size() ; j++){
+                c.faces.at(i).vts.at(j) = project(c.faces.at(i).vts.at(j), FOV);
+                c.faces.at(i).vts.at(j) =  screen(c.faces.at(i).vts.at(j), W, H);
+                //vts_cp.at(j) = screen(vts_cp.at(j), W, H);
+            };
+        };
+        */
+
+        // render the 2 cube faces separately
+        for(int i = 0 ; i < faces.size(); i++){
+            for(int j = 0 ; j < faces.at(i).vts.size(); j++){
+                Vertex v1 = screen(project(faces.at(i).vts.at(j), FOV), W, H);
+                Vertex v2 = screen(project(faces.at(i).vts.at((j+1) % faces.at(i).vts.size()), FOV), W, H);
+                SDL_RenderLine(renderer, v1.x, v1.y, v2.x, v2.y);
+            }
+        }
+
+        // connect the 2 cube faces
+        for(int i = 0; i < faces.at(0).vts.size(); i++){
+            Vertex v1 = screen(project(faces.at(0).vts.at(i), FOV), W, H);
+            Vertex v2 = screen(project(faces.at(1).vts.at(i), FOV), W, H);
+            SDL_RenderLine(renderer, v1.x, v1.y, v2.x, v2.y);    
+        }  
+       // 
+       //printProperties();
+    };
+};
+
+//
+//
+//
+//
+//
 int main(void){
 
     // SDL_SetHint(SDL_HINT_VIDEO_DRIVER, "x11");
     SDL_Init(SDL_INIT_VIDEO);
     //   
-    int W = 800;
-    int H = 800;
     SDL_Window* window = SDL_CreateWindow("spinny cube", W, H, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
  
-    /* Vertex data for a unit cube 
-    * - vertex data relative to camera (i.e., camera space -> camera is at (0,0,0))
-    */ 
-    // offset == base gives a cube
-    const float offset = 0.5; // controls depth of cuboid (0 for flat rectangle)
-    const float base = 0.5; // controls width + height of cuboid (smaller base -> smaller width & height)
-    Vertex v1 = {.x =  base, .y =  base, .z =  - offset, .w = 1};
-    Vertex v2 = {.x = -base, .y =  base, .z =  - offset, .w = 1};
-    Vertex v3 = {.x = -base, .y = -base, .z =  - offset, .w = 1};
-    Vertex v4 = {.x =  base, .y = -base, .z =  - offset, .w = 1};
-    Vertex v5 = {.x =  base, .y =  base, .z =    offset, .w = 1};
-    Vertex v6 = {.x = -base, .y =  base, .z =    offset, .w = 1};
-    Vertex v7 = {.x = -base, .y = -base, .z =    offset, .w = 1};
-    Vertex v8 = {.x =  base, .y = -base, .z =    offset, .w = 1};
-   
+
+
+    /* 
+     *
+     * World coordinates
+     *
+    */
+    const float offset = 0.5;
+    const float base   = 0.5;
+    const Vertex cube_center = Vertex {.x = 0.7, .y = 0.85, .z = 3, .w = 1};
+    // const Vertex cube_center = Vertex {.x = 0, .y = 0, .z = 0, .w = 1};
+    Vertex v1 = {.x =  cube_center.x + base, .y =  cube_center.y + base, .z =  cube_center.z - offset, .w = 1};
+    Vertex v2 = {.x =  cube_center.x - base, .y =  cube_center.y + base, .z =  cube_center.z - offset, .w = 1};
+    Vertex v3 = {.x =  cube_center.x - base, .y =  cube_center.y - base, .z =  cube_center.z - offset, .w = 1};
+    Vertex v4 = {.x =  cube_center.x + base, .y =  cube_center.y - base, .z =  cube_center.z - offset, .w = 1};
+    Vertex v5 = {.x =  cube_center.x + base, .y =  cube_center.y + base, .z =  cube_center.z + offset, .w = 1};
+    Vertex v6 = {.x =  cube_center.x - base, .y =  cube_center.y + base, .z =  cube_center.z + offset, .w = 1};
+    Vertex v7 = {.x =  cube_center.x - base, .y =  cube_center.y - base, .z =  cube_center.z + offset, .w = 1};
+    Vertex v8 = {.x =  cube_center.x + base, .y =  cube_center.y - base, .z =  cube_center.z + offset, .w = 1};
+    const Vertex CAMERA_POS = Vertex {.x = 0, .y = 0, .z = 0, .w = 1}; // camera placed at origin of world coord system for convenience
+
     /*
     * vts -> transformations in camera space are applied here
     * vts_cp -> will be used to store each iterations projected + screen-mapped coordinates for rendering
     *
-    */ 
-    std::vector<Vertex> vts = {
-        v1,v2,v3,v4, // 2 faces 
-        v5,v6,v7,v8
-    };
-    std::vector<Vertex> vts_cp = {
-        v1,v2,v3,v4, // 2 faces 
-        v5,v6,v7,v8
-    };
+    */
+    Face f1    = std::vector<Vertex> {v1,v2,v3,v4}; 
+    Face f2    = std::vector<Vertex> {v5,v6,v7,v8}; 
+    std::vector<Face> faces = {f1, f2};
+    
+    Face f1_cp    = std::vector<Vertex> {v1,v2,v3,v4}; 
+    Face f2_cp    = std::vector<Vertex> {v5,v6,v7,v8}; 
+    std::vector<Face> faces_cp = {f1_cp, f2_cp};
+    Cube c = Cube(faces_cp); 
+
 
     //
     SDL_Event event;
     bool running = true;
     const float FPS = 60;
-    const float FOV = 90;
     while(running) {
+        // User input
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_EVENT_QUIT){
                running = false;
             }
         }
-        //
+   
+        // Reset screen to all black
         SDL_SetRenderDrawColor(renderer, 0, 0, 0,SDL_ALPHA_OPAQUE); // Black
         SDL_RenderClear(renderer);
-
-
-        /*
-        * Apply transformations in camera space to vertices in camera space coords
-        */
-        for(int i = 0 ; i < vts.size(); i++){
-	    vts.at(i) = rot(vts.at(i), 1, X_AXIS); // rotation relative to camera (vts coordinates are relative to the camera, cam at origin)
-            vts.at(i) = rot(vts.at(i), 1, Y_AXIS); // rotation relative to camera (vts coordinates are relative to the camera, cam at origin)
-
-            // draw point that is being rotates around
-            SDL_SetRenderDrawColor(renderer, 245, 66, 197, SDL_ALPHA_OPAQUE); // Pink
-            
-            float x_off = -0.7;
-            float y_off = -0.85;
-            float z_off = -3;  // camera translation along the axes
-            Vertex rotPoint = screen(project({.x = -x_off, .y = -y_off, .z = -z_off, .w = 1}, FOV), W, H);
-            SDL_RenderPoint(renderer, rotPoint.x, rotPoint.y);
-
-            // move camera away (here, we move all objects in the scene in the *opposite* direction of the camera to achieve this)
-            
-            for(int i =0 ; i < vts.size() ; i++){
-               Vertex v = vts.at(i);
-               Vertex offset_vector = Vertex {.x = -x_off, .y = -y_off, .z = -z_off, .w = 0};
-               Vertex v_t = translate(v, offset_vector);
-               vts_cp.at(i) = v_t;
-            };
-
-        }
+        //
         
         /*
-        * Transition to screen coordinates for rendering
+         * 1. Translate from world space to camera space
+         * 2. Apply transformations in camera space
+         * 3. Translate back to world space
         */
-        SDL_SetRenderDrawColor(renderer, 10, 245, 37,SDL_ALPHA_OPAQUE); // Green
-        for(int i =0 ; i < vts.size() ; i++){
-           vts_cp.at(i) = project(vts_cp.at(i), FOV);
-           vts_cp.at(i) = screen(vts_cp.at(i), W, H);
-        };
-                
-        const int half = vts.size() / 2; 
-        for(int i = 0 ; i < half ; i++){
-            // face 1
-            SDL_RenderLine(renderer, vts_cp.at(i).x,      vts_cp.at(i).y,      vts_cp.at((i+1) % half).x,        vts_cp.at((i+1) % half).y);
-      
-            // face 2
-            SDL_RenderLine(renderer, vts_cp.at(i+half).x, vts_cp.at(i+half).y, vts_cp.at((i+1) % half + half).x, vts_cp.at((i+1) % half + half).y);
-            
-            // connect the 2 faces
-            SDL_RenderLine(renderer, vts_cp.at(i).x, vts_cp.at(i).y, vts_cp.at((i+ half)).x, vts_cp.at((i + half)).y);
-        }
-      
+        c.translate_geom(Vertex {.x = -cube_center.x, .y = -cube_center.y, .z = -cube_center.z}); 
+        c.rotate_geom(1, X_AXIS);
+        c.rotate_geom(1, Y_AXIS);
+        c.translate_geom(Vertex {.x = cube_center.x, .y = cube_center.y, .z = cube_center.z});
+        
+        /*
+         * All transformations/mutations are done, simply render the vertices
+         */
+        c.printProperties();
+        c.render(renderer);
+        
+        //
         SDL_RenderPresent(renderer);
-        // printVertices(vts_cp); 
         SDL_Delay(1000 / FPS);
     }
 
